7efdd96b8ad1931f7cbf561a53ac83bb4178265d
0ac_raf0dev <135514115+En-achraf@users.noreply.github.com>
2024-09-30 17:23:52 +0100

    70%

--- ft_printf.c
+++ ft_printf.c
@@ -1,8 +1,8 @@
 #include "ft_printf.h"
 
-static int	lenthstr(char *a)
+static int lenthstr(char *a)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (a[i])
@@ -12,9 +12,9 @@ static int	lenthstr(char *a)
 	return (i);
 }
 
-static int	numlenth(int a)
+static int numlenth(int a)
 {
-	int	count;
+	int count;
 
 	count = 0;
 	if (a <= 0)
@@ -27,9 +27,9 @@ static int	numlenth(int a)
 	return (count);
 }
 
-static int	numlenth_hex(unsigned long num)
+static int numlenth_hex(unsigned long num)
 {
-	int	len;
+	int len;
 
 	len = 0;
 	if (num == 0)
@@ -42,27 +42,49 @@ static int	numlenth_hex(unsigned long num)
 	return (len);
 }
 
-int	ft_printf(const char *format, ...)
+static int unslenth_num(unsigned int nb)
 {
-	int				i;
-	int				lenth_result;
-	va_list			arg;
-	char			ch_fo;
-	char			*str_fo;
-	int				dec_fo;
-	unsigned long	ptr_val;
+	int count;
 
-	i = 0;
-	lenth_result = 0;
-	va_start(arg, format);
-	while (format[i])
+	count = 0;
+	if (nb == 0)
+		count = 1;
+	while (nb)
 	{
-		if (format[i] == '%')
-		{
-			i++;
-			// for the char type
-			if (format[i] == 'c')
+		nb /= 10;
+		count++;
+	}
+	return (count);
+}
+
+static void lowhex_value(int x)
+{
+	char *hex = "0123456789abcdef";
+	if (x >= 16)
+		lowhex_value(x / 16);
+	ft_putchar_fd(hex[x % 16], 1);
+}
+
+static void uphex_value(int x)
+{
+	char *hex = "0123456789ABCDEF";
+	if (x >= 16)
+		uphex_value(x / 16);
+	ft_putchar_fd(hex[x % 16], 1);
+}
+
+static void set_fornat (const char *format, int i, int lenth_result, va_list arg) {
+	char ch_fo;
+	char *str_fo;
+	int dec_fo;
+	unsigned long ptr_val;
+	unsigned int unsdec_fo;
+	unsigned int lowhex_fo;
+	unsigned int uphex_fo;
+	
+	if (format[i] == 'c')
 			{
+				//for the char type
 				ch_fo = (char)va_arg(arg, int);
 				ft_putchar_fd(ch_fo, 1);
 				lenth_result++;
@@ -74,7 +96,7 @@ int	ft_printf(const char *format, ...)
 				ft_putstr_fd(str_fo, 1);
 				lenth_result += lenthstr(str_fo);
 			}
-			else if (format[i] == 'd')
+			else if (format[i] == 'd' || format[i] == 'i')
 			{
 				// for integer type
 				dec_fo = va_arg(arg, int);
@@ -83,7 +105,6 @@ int	ft_printf(const char *format, ...)
 			}
 			else if (format[i] == 'p')
 			{
-				//this for the &
 				// Get the pointer and cast it to unsigned long
 				ptr_val = (unsigned long)va_arg(arg, void *);
 				ft_putstr_fd("0x", 1);
@@ -93,6 +114,48 @@ int	ft_printf(const char *format, ...)
 					ft_puthex_fd(ptr_val, 1);
 				lenth_result += 2 + numlenth_hex(ptr_val);
 			}
+			else if (format[i] == 'u')
+			{
+				unsdec_fo = va_arg(arg, unsigned int);
+				ft_putunsnbr_fd(unsdec_fo, 1);
+				lenth_result += unslenth_num(unsdec_fo);
+			}
+			else if (format[i] == 'x')
+			{
+				lowhex_fo = va_arg(arg, unsigned int);
+				lowhex_value(lowhex_fo);
+				lenth_result += numlenth_hex(lowhex_fo);
+			}
+			else if (format[i] == 'X')
+			{
+				uphex_fo = va_arg(arg, unsigned int);
+				uphex_value(uphex_fo);
+				lenth_result += numlenth_hex(uphex_fo);
+			}
+			else if (format[i] == '%')
+			{
+				ft_putchar_fd('%', 1);
+				lenth_result++;
+				
+			}
+}
+
+int ft_printf(const char *format, ...)
+{
+	int i;
+	int lenth_result;
+	va_list arg;
+
+	i = 0;
+	lenth_result = 0;
+	va_start(arg, format);
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			// for the format type
+			set_fornat(format, i, lenth_result, arg);
 		}
 		else
 		{
@@ -103,15 +166,4 @@ int	ft_printf(const char *format, ...)
 	}
 	va_end(arg);
 	return (lenth_result);
-}
-
-#include <stdio.h>
-
-int	main(void)
-{
-	char *p;
-	p = "hello world";
-
-	ft_printf("this is the a char : %d %c %c %s %p \n", 6, 'b', 'n',"hello world", &p);
-	printf("this is the a char : %d %c %c %s %p \n", 6, 'b', 'n',"hello world", &p);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -9,5 +9,7 @@ void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_puthex_fd(unsigned long n, int fd);
+void    ft_putunsnbr_fd(unsigned int nb, int fd);
+int	ft_printf(const char *format, ...);
 
 #endif
